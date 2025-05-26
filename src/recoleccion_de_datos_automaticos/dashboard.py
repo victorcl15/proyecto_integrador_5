c96bea7e5775a05315f771f48020e24ab9643e05
Victor.calle <victor.calle@est.iudigital.edu.co>
2025-05-25 21:15:09 -0500

    eliminar doble importacion de matplotlib y quitar paso para ejecutar dashboard.py

--- src/recoleccion_de_datos_automaticos/dashboard.py
+++ src/recoleccion_de_datos_automaticos/dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Cargar los datos
